41b4c9ae27bf4dce7313fb42c41b2564947dd6d8
league <league@wts8.attlocal.net>
2019-02-07 19:58:34 -0800

    GetLatestTweet

--- src/LatestTweet.java
+++ src/LatestTweet.java
@@ -1,4 +1,7 @@
-import java.awt.Frame;
+
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -6,19 +9,91 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class LatestTweet {
-JFrame frame = new JFrame();
-JLabel label = new JLabel();
-JPanel panel = new JPanel();
-JButton button = new JButton();
-JTextField textfield = new JTextField();
-public static void main(String[] args) {
-System.out.println("Tweet, Tweet");
-LatestTweet tweets = new LatestTweet();
-tweets.tweet();
-}
-public void tweet() {
-	frame.setVisible(true);
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+public class LatestTweet implements MouseListener{
 	
-}
+	JFrame frame = new JFrame();
+	JLabel label = new JLabel();
+	JPanel panel = new JPanel();
+	JButton button = new JButton();
+	JTextField textfield = new JTextField(15);
+
+	public static void main(String[] args) {
+		System.out.println("Tweet, Tweet");
+		LatestTweet tweets = new LatestTweet();
+		tweets.tweet();
+	}
+
+	public void tweet() {
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(panel);
+		panel.add(button);
+		panel.add(textfield);
+		panel.add(label);
+		button.setText("Search the Twitterverse");
+		textfield.setText("oof");
+		button.addMouseListener(this);
+		
+		frame.pack();
+		
+	}
+	private String getLatestTweet(String searchingFor) {
+
+	      Twitter twitter = new TwitterFactory().getInstance();
+
+	      AccessToken accessToken = new AccessToken(
+	            "2453751158-IVD2VGZsvwZiRKxNe3Gs2lMjg30nvSkV1xSuPFf",
+	            "vBa5PjKfuMTK1LLBG51nCUI9r5d6Ph5cAHrS73spg6Nvu");
+
+	      twitter.setOAuthConsumer("YqeZdD2hYxOKv4QOkmp0i2djN",
+	            "6XUB1r8KXBvd8Pk9HHW3NgphMxHvHWBLAr5TihnckMU0ttyGST");
+
+	      twitter.setOAuthAccessToken(accessToken);
+
+	      Query query = new Query(searchingFor);
+	      try {
+	            QueryResult result = twitter.search(query);
+	            return result.getTweets().get(0).getText();
+	      } catch (Exception e) {
+	            System.err.print(e.getMessage());
+	            return "What the heck is that?";
+	      }
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		String tweet = getLatestTweet(textfield.getText());
+		System.out.println(tweet);
+	}
+	
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
