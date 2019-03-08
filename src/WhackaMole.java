3b4673e820f4e7470e30506081df275fdb6ad7b8
league <league@wts8.attlocal.net>
2019-03-07 19:55:02 -0800

    WhackaMole IP

--- src/WhackaMole.java
+++ src/WhackaMole.java
@@ -1,3 +1,5 @@
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.Random;
 
 import javax.swing.JButton;
@@ -6,40 +8,71 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class WhackaMole {
-JFrame frame = new JFrame();
-JPanel panel = new JPanel();
-JLabel label = new JLabel();
-JButton button = new JButton();
+public class WhackaMole implements MouseListener {
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JLabel label = new JLabel();
 
 	public static void main(String[] args) {
-	WhackaMole mole = new WhackaMole();
-	mole.whack();
-	
-}
+		WhackaMole mole = new WhackaMole();
+		mole.whack();
+
+	}
 
 	public void whack() {
-	frame.setVisible(true);
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	frame.add(panel);
-	drawButtons();
-	panel.add(label);
-	frame.setSize(600, 175);
-	
-	
-}
+		frame =  new JFrame();
+		panel = new JPanel();
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(panel);
+		drawButtons();
+		panel.add(label);
+		frame.setSize(600, 175);
+		// .addMouseListener(this);
+
+	}
+
 	public void drawButtons() {
 		int whacks = new Random().nextInt(24);
-		for(int i=0; i<24; i++) {
-			if(i == whacks) {
-				panel.add(new JButton("Mole!"));
-			} else {
-			panel.add(new JButton());
+		for (int i = 0; i < 24; i++) {
+			JButton button = new JButton();
+			button.addMouseListener(this);
+			panel.add(button);
+			if (i == whacks) {
+				button.setText("Mole!");
 			}
 		}
-		
-		
-		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		//System.out.println("hi");
+		frame.dispose();
+		whack();
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
 	}
 }
-
